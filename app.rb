87b4aeed81695e27707cd825147409f011fa4e96
Alfoldi <balfoldi@yahoo.fr>
2020-07-24 14:11:26 +0200

    macro_commit_initiatizated

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'dotenv'
+require 'csv'
+require 'json'
+require 'nokogiri'
+require 'google_drive'
+require 'launchy'
+require 'pry'
+require 'open-uri'
+require 'time'
+ require'date'
+Bundler.require
